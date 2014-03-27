59dc9100c30b27f97ce56bb6fd05f0906d1f34e0
Javier Gomez <alambike@cpan.org>
2014-03-27 23:56:41 +0000

    Import of ALAMBIKE/Eixo-Base-1.01 from CPAN.
    gitpan-cpan-distribution: Eixo-Base
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         ALAMBIKE/Eixo-Base-1.01.tar.gz
    gitpan-cpan-author:       ALAMBIKE
    gitpan-cpan-maturity:     released

---
+++ lib/Eixo/Base.pm
@@ -0,0 +1,33 @@
+package Eixo::Base;
+
+use 5.008001;
+use strict;
+use warnings;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration	use Eixo::Base ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw(
+	
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+	
+);
+
+our $VERSION = '1.01';
+
+
+# Preloaded methods go here.
+
+1;
